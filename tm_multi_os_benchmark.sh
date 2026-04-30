b21bcf3474a67b02f6a4a13c8f9fbea5015df3df
danishkhan0101 <danishfxunited@gmail.com>
2026-04-30 10:50:53 +0800

    Update tm_multi_os_benchmark.sh

--- tm_multi_os_benchmark.sh
+++ tm_multi_os_benchmark.sh
@@ -387,11 +387,12 @@ run_phase_1() {
     for IP in "${WINDOWS_MACHINES[@]}"; do
         if [ "$RUN_CIS" == true ]; then
             echo -e "${CYAN}🔍 [WINDOWS - CIS] Scanning $IP...${NC}"
-            CHEF_LICENSE="accept-silent" /usr/bin/inspec exec $WIN_CIS_BENCHMARK -t winrm://${IP} --user="${AUDIT_USER}" --password="${AUDIT_PASS}" --reporter cli json:heimdall_before_CIS_WIN_${IP}.json || true
+            # 🚨 THE FIX: Added .\ to the username to force Local Auth, and added --insecure
+            CHEF_LICENSE="accept-silent" /usr/bin/inspec exec $WIN_CIS_BENCHMARK -t winrm://${IP} --user=".\${AUDIT_USER}" --password="${AUDIT_PASS}" --insecure --reporter cli json:heimdall_before_CIS_WIN_${IP}.json || true
         fi
         if [ "$RUN_TM" == true ]; then
             echo -e "${CYAN}🔍 [WINDOWS - TM] Scanning $IP...${NC}"
-            CHEF_LICENSE="accept-silent" /usr/bin/inspec exec $WIN_CUSTOM_BENCHMARK -t winrm://${IP} --user="${AUDIT_USER}" --password="${AUDIT_PASS}" --reporter cli json:heimdall_before_TM_WIN_${IP}.json || true
+            CHEF_LICENSE="accept-silent" /usr/bin/inspec exec $WIN_CUSTOM_BENCHMARK -t winrm://${IP} --user=".\${AUDIT_USER}" --password="${AUDIT_PASS}" --insecure --reporter cli json:heimdall_before_TM_WIN_${IP}.json || true
         fi
     done
 }
