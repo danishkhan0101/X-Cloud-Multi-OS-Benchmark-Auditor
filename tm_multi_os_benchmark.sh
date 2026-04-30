5891d952a3012671b3fef0a84a223dbc73f34925
danishkhan0101 <danishfxunited@gmail.com>
2026-04-30 11:01:10 +0800

    Update tm_multi_os_benchmark.sh

--- tm_multi_os_benchmark.sh
+++ tm_multi_os_benchmark.sh
@@ -387,7 +387,6 @@ run_phase_1() {
     for IP in "${WINDOWS_MACHINES[@]}"; do
         if [ "$RUN_CIS" == true ]; then
             echo -e "${CYAN}🔍 [WINDOWS - CIS] Scanning $IP...${NC}"
-            # 🚨 THE FIX: Added .\ to the username to force Local Auth, and added --insecure
             CHEF_LICENSE="accept-silent" /usr/bin/inspec exec $WIN_CIS_BENCHMARK -t winrm://${IP} --user=".\${AUDIT_USER}" --password="${AUDIT_PASS}" --insecure --reporter cli json:heimdall_before_CIS_WIN_${IP}.json || true
         fi
         if [ "$RUN_TM" == true ]; then
@@ -485,11 +484,11 @@ run_phase_4() {
     for IP in "${WINDOWS_MACHINES[@]}"; do
         if [ "$RUN_CIS" == true ]; then
             echo -e "${CYAN}✅ [WINDOWS - CIS] Verifying $IP...${NC}"
-            CHEF_LICENSE="accept-silent" /usr/bin/inspec exec $WIN_CIS_BENCHMARK -t winrm://${IP} --user="${AUDIT_USER}" --password="${AUDIT_PASS}" --reporter cli json:heimdall_after_CIS_WIN_${IP}.json || true
+            CHEF_LICENSE="accept-silent" /usr/bin/inspec exec $WIN_CIS_BENCHMARK -t winrm://${IP} --user=".\${AUDIT_USER}" --password="${AUDIT_PASS}" --insecure --reporter cli json:heimdall_after_CIS_WIN_${IP}.json || true
         fi
         if [ "$RUN_TM" == true ]; then
             echo -e "${CYAN}✅ [WINDOWS - TM] Verifying $IP...${NC}"
-            CHEF_LICENSE="accept-silent" /usr/bin/inspec exec $WIN_CUSTOM_BENCHMARK -t winrm://${IP} --user="${AUDIT_USER}" --password="${AUDIT_PASS}" --reporter cli json:heimdall_after_TM_WIN_${IP}.json || true
+            CHEF_LICENSE="accept-silent" /usr/bin/inspec exec $WIN_CUSTOM_BENCHMARK -t winrm://${IP} --user=".\${AUDIT_USER}" --password="${AUDIT_PASS}" --insecure --reporter cli json:heimdall_after_TM_WIN_${IP}.json || true
         fi
     done
 }
